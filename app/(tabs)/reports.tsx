f67bda5d8bf691a45f9d39901c5ed71fb64a4aef
loso <147423869+KhalisahNajah@users.noreply.github.com>
2025-07-01 01:11:02 +0800

    Updated reports.tsx

--- app/(tabs)/reports.tsx
+++ app/(tabs)/reports.tsx
@@ -816,7 +816,7 @@ const createStyles = (colors: any, theme: string) => StyleSheet.create({
   },
   reportsList: {
     flex: 0.5,
-    paddingTop: 5, // You might want to adjust this further (e.g., to 0 or 5)
+    paddingTop: 10, // You might want to adjust this further (e.g., to 0 or 5)
                     // depending on how much gap you want between filters and first report.
     paddingHorizontal: 20,
    
